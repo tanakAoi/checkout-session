f90520f84e21ef9c3d599bcaffa9207674c18125
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-04-04 15:00:24 +0200

    feat : Add general layout

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -4,7 +4,7 @@ export const Layout = () => {
   return (
     <>
       <header></header>
-      <main>
+      <main className="min-h-screen flex flex-col items-center justify-center">
         <Outlet />
       </main>
       <footer></footer>
