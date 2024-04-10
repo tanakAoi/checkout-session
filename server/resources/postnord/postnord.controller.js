fa38760e3855994c4c92927ad3d30683989a278d
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-04-10 11:55:44 +0200

    fix : Fetch only 5 service points from Postnord API

--- server/resources/postnord/postnord.controller.js
+++ server/resources/postnord/postnord.controller.js
@@ -4,7 +4,7 @@ const servicePoints = async (req, res) => {
   const { city, postalCode, street } = req.body;
   try {
     const response = await axios.get(
-      `http://atapi2.postnord.com/rest/businesslocation/v5/servicepoints/nearest/byaddress?apikey=${process.env.POSTNORD_KEY}&returnType=json&countryCode=SE&agreementCountry=SE&city=${city}&postalCode=${postalCode}&streetName=${street.streetName}&streetNumber=${street.streetNumber}&numberOfServicePoints=10&srId=EPSG:4326&context=optionalservicepoint&responseFilter=public`
+      `http://atapi2.postnord.com/rest/businesslocation/v5/servicepoints/nearest/byaddress?apikey=${process.env.POSTNORD_KEY}&returnType=json&countryCode=SE&agreementCountry=SE&city=${city}&postalCode=${postalCode}&streetName=${street.streetName}&streetNumber=${street.streetNumber}&numberOfServicePoints=5&srId=EPSG:4326&context=optionalservicepoint&responseFilter=public`
     );
     res.status(200).json(response.data)
   } catch (error) {
