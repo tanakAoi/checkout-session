06b7c9bf3fe0294d04ed00b656af1ed699fa3a52
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-04-13 15:47:39 +0200

    perf : Add error handling

--- server/resources/orders/orders.controller.js
+++ server/resources/orders/orders.controller.js
@@ -3,12 +3,20 @@ const fs = require("fs").promises;
 const fetchOrders = async (req, res) => {
   const { stripeId } = req.body;
 
-  const ordersData = await fs.readFile("./data/orders.json");
-  const orders = JSON.parse(ordersData);
+  try {
+    const ordersData = await fs.readFile("./data/orders.json");
+    const orders = JSON.parse(ordersData);
+  
+    const userOrders = orders.filter((order) => {
+      return order.customer.stripeId === stripeId;
+    });
+  
+    res.status(200).json(userOrders);
+  } catch (error) {
+    console.error("Error", error)
+    res.status(400).json("Could not fetch orders.")
+  }
 
-  const userOrders = orders.filter((o) => o.stripeId === stripeId);
-
-  res.status(200).json(userOrders);
 };
 
 module.exports = { fetchOrders };
