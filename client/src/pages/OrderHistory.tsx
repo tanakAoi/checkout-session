8b357462249f9dab04ea93fa76290d5cabb1edde
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-04-14 13:48:49 +0200

    feat : Add sorting function

--- client/src/pages/OrderHistory.tsx
+++ client/src/pages/OrderHistory.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useUser } from "../contexts/UserContext";
 import { Button } from "../components/Button";
 import { IOrder } from "../models/IOrder";
@@ -9,6 +9,7 @@ export const OrderHistory = () => {
   const [orders, setOrders] = useState<IOrder[]>(
     JSON.parse(localStorage.getItem("orders") || "[]")
   );
+  const [sortBy, setSortBy] = useState("default");
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -28,13 +29,35 @@ export const OrderHistory = () => {
     fetchOrders();
   }, [isLoggedIn]);
 
+  const handleSortChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSortBy(e.target.value);
+  };
+
+  const sortOrders = (sortBy: string) => {
+    const sortedOrders = [...orders].sort((a, b) => {
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
+      return sortBy === "latest" ? dateB - dateA : dateA - dateB;
+    });
+    return sortedOrders;
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center gap-5 py-20">
       <h2 className="text-3xl pb-10">Order History</h2>
       <div className="flex flex-col gap-5">
+        <select
+          className="select select-bordered max-w-xs"
+          value={sortBy}
+          onChange={(e) => handleSortChange(e)}
+        >
+          <option disabled selected value="default">Sort by:</option>
+          <option value="latest">Latest</option>
+          <option value="oldest">Oldest</option>
+        </select>
         {isLoggedIn ? (
           orders.length > 0 ? (
-            orders.map((order) => (
+            sortOrders(sortBy).map((order) => (
               <div
                 key={order.orderNumber}
                 className="collapse collapse-plus bg-coffee"
