70d733fc08ef8f721db1732812e106a22b1b15c9
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-04-14 13:47:30 +0200

    chore : Formatted

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,48 +1,59 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 interface IBtnProps {
-    linkTo?: string;
-    children: string;
-    className?: string;
-    disabled?: boolean; 
-    selected?: boolean,
-    size: "sm" | "md" | "lg" | "xl";
-    color: "light" | "dark" | "transparent";
-    event?: (e: any) => void;
+  linkTo?: string;
+  children: string;
+  className?: string;
+  disabled?: boolean;
+  selected?: boolean;
+  size: "sm" | "md" | "lg" | "xl";
+  color: "light" | "dark" | "transparent";
+  event?: (e: any) => void;
 }
 
 const style = {
-    sizes: {
-        sm: "text-sm px-6",
-        md: "text-base px-10",
-        lg: "text-lg px-14",
-        xl: "text-xl px-16"
-    },
-    colors: {
-        light: `bg-leaf text-light border-transparent border-2 hover:bg-light hover:text-leaf hover:border-leaf hover:border-2`,
-        dark: `bg-neutral-950 text-primary border-neutral-950 hover:bg-neutral-50 hover:text-primary`,
-        transparent: `text-primary shadow-none bg-transparent border-none hover:bg-neutral-950 hover:text-primary`,
-        darkSelected: `bg-neutral-950 text-primary border-neutral-950 hover:bg-neutral-950 hover:border-neutral-950`
-    }
-}
+  sizes: {
+    sm: "text-sm px-6",
+    md: "text-base px-10",
+    lg: "text-lg px-14",
+    xl: "text-xl px-16",
+  },
+  colors: {
+    light: `bg-leaf text-light border-transparent border-2 hover:bg-light hover:text-leaf hover:border-leaf hover:border-2`,
+    dark: `bg-neutral-950 text-primary border-neutral-950 hover:bg-neutral-50 hover:text-primary`,
+    transparent: `text-primary shadow-none bg-transparent border-none hover:bg-neutral-950 hover:text-primary`,
+    darkSelected: `bg-neutral-950 text-primary border-neutral-950 hover:bg-neutral-950 hover:border-neutral-950`,
+  },
+};
 
-export const Button = ({linkTo, children, className = "", disabled = false, selected = false, size = "md", color = "light", event}: IBtnProps) => {
-
-    const buttonContent = (
-        <button
-            className={`
-                btn 
+export const Button = ({
+  linkTo,
+  children,
+  className = "",
+  disabled = false,
+  selected = false,
+  size = "md",
+  color = "light",
+  event,
+}: IBtnProps) => {
+  const buttonContent = (
+    <button
+      className={`
+            btn
                 ${className} 
                 ${style.sizes[size]} 
-                ${selected ? "selected" && style.colors.darkSelected : style.colors[color]}
+                ${
+                  selected
+                    ? "selected" && style.colors.darkSelected
+                    : style.colors[color]
+                }
             `}
-            disabled={disabled}
-            onClick={event}
-        >
-            {children}
-        </button>
-    )
+      disabled={disabled}
+      onClick={event}
+    >
+      {children}
+    </button>
+  );
 
-    return linkTo ? <Link to={linkTo}>{buttonContent}</Link> : buttonContent
-    
-}
+  return linkTo ? <Link to={linkTo}>{buttonContent}</Link> : buttonContent;
+};
